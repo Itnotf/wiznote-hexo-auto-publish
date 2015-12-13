a1073823795457318b8d9732a53a10290c6ef4ed
60 <1096364408@qq.com>
2015-12-14 02:15:09 +0800

    直接在wiz中使用md格式 yaml由传过去的参数生成

--- wiz_blog.php
+++ wiz_blog.php
@@ -1,35 +1,58 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Yaml\Yaml;
 
 $path_to_tmp = sys_get_temp_dir() . '/hexo/';
 
 $input = file_get_contents('php://input', 'r');
-$xml   = simplexml_load_string($input);
 
-$params = $xml->params;
+$xml = new DOMDocument();
 
-$params_struct = $params->param[3]->value->struct;
+//$input ? $xml->loadXML($input) && $xml->save('test.xml') : $xml->load('test.xml');;
+empty($input) ? die('empty input!') : $xml->loadXML($input);
 
-$content    = $params_struct->member[1]->value->string;
-$file_title = html_entity_decode($params_struct->member[4]->value->string, ENT_COMPAT, 'UTF-8');
+$params = $xml->getElementsByTagName('param');
 
-//file_put_contents($file_title.'.html', $content);
+$struct = $params->item(3);
 
-$text = preg_replace('/<div>(.*?)<\/div>/', "$1\n", $content);
+$member = $struct->getElementsByTagName('member');
+
+$member_arr = array();
+foreach ($member as $v) {
+    $member_name              = $v->getElementsByTagName('name')->item(0)->nodeValue;
+    $member_arr[$member_name] = $v->getElementsByTagName('value')->item(0)->nodeValue;
+}
+
+$content = $member_arr['description'];
+
+$text = preg_replace('/<div>(.*?)<\/div>/', "$1" . PHP_EOL, $content);//<div>变换行
 $text = html_entity_decode($text);
-$text = preg_replace("/<.*?>/", "", $text);
-$text = preg_replace('/(.*)\*\*\*BEGIN\*\*\*(.*)\*\*\*END\*\*\*.*$/s', '$1$2', $text);
+$text = preg_replace("/<.*?>/", "", $text);//去掉一些类似<a/>的标签
+$text = preg_replace('/来自为知笔记\(Wiz\)/', "", $text);
 
-$ret = file_put_contents($path_to_tmp . $file_title . '.md', $text);
+//hexo 的正文是yaml格式
+$yaml['title']    = $member_arr['title'];
+$yaml['date']     = date('Y-m-d H:i:s', strtotime($member_arr['dateCreated']));
+$yaml['category'] = '日志';
+$yaml['tags']     = explode(', ', $member_arr['mt_keywords']);
+//$hexo['description'] = $member_arr[''];
+
+$post = implode('---' . PHP_EOL, array(Yaml::dump($yaml), $text));
+
+$ret = file_put_contents($path_to_tmp . $yaml['title'] . '.md', $post);
 
 //在tmp文件夹下保存一下日志的备份
-$xml->asXML($path_to_tmp . $file_title . '.xml');
+$xml->save($path_to_tmp . $yaml['title'] . '.xml');
 unset($xml);
 
+
+exit;
 
 $xml = new XMLWriter();
 $xml->openUri('php://output'); // or 'php://output' 
 $xml->setIndentString("  ");
-$xml->setIndent(true);
+$xml->setIndent(TRUE);
 // start 
 $xml->startDocument('1.0', 'ISO-8859-1');
 // <methodResponse> 
